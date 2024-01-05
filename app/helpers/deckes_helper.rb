f1e71ada101b2038b20c824f69c6bb01e7888372
kajigii <wata15708@gmail.com>
2024-01-05 16:11:02 +0900

    フロント実装の準備

---
+++ app/helpers/deckes_helper.rb
@@ -0,0 +1,2 @@
+module DeckesHelper
+end
--- db/migrate/20240105064543_create_games.rb
+++ db/migrate/20240105064543_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
-
+      t.name
       t.timestamps
     end
   end
