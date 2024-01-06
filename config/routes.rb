8ae36f0f862c3d46498fbc924535b3299a3c7f2d
kajigii <wata15708@gmail.com>
2024-01-06 15:24:23 +0900

    Vanguardページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'games/index'
+  # get 'games/index'
   root to: "games#index"
+
+  # ルーティングはすべてのビュー作成後に繋げる(モデル整理のため)
+  # ヴァンガードへのルートを設定
+  # get '/vanguard', to: 'vanguard#choose_rules', as: 'vanguard_path'
+  resources :vanguardes, only: [:index]
+
+  # バトスピへのルートを設定
+  get '/battle_spirits', to: 'battle_spirits#choose_color', as: 'battle_spirits_path'
 end
